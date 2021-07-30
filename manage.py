567a0216526547cde49e23d07bc080cb4db0b6d6
walterxue@outlook.com <walterxue@outlook.com>
2021-07-30 17:44:35 +0200

    add zone_file

---
+++ manage.py
@@ -0,0 +1,9 @@
+from flask_script import Manager,Server
+from IncrteCloudAPI import create_app
+
+app = create_app()
+manager = Manager(app)
+
+if __name__ == "__main__":
+    # app.config['SECRET_KEY'] = "test"
+    manager.run()
